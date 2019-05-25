a8c9079f8183f09e21304099313ed198e467e0a7
Banyawat Kaewsamer <banyawat@thinknet.co.th>
2019-05-25 19:29:01 +0700

    try to use css import

--- pages/schedule.js
+++ pages/schedule.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react'
 import { Button } from 'reactstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 
 export default class schedule extends Component {
   render() {
     return (
       <div>
-        <h1>Schedule</h1>
+        <h1>ตารางการเผา</h1>
         <Button>Test</Button>
+        <div>
+        </div>
       </div>
     )
   }
