d1ea9e5d312b0c55cbf2861d379207c318295e8d
Banyawat Kaewsamer <banyawat@thinknet.co.th>
2019-05-25 19:14:56 +0700

    add react strap

---
+++ pages/schedule.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import { Button } from 'reactstrap'
+
+export default class schedule extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Schedule</h1>
+        <Button>Test</Button>
+      </div>
+    )
+  }
+}
