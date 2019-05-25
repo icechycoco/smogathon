1d89c2a084afc7f6346617f74026d90cc5281f48
icechycoco <icechycoco@gmail.com>
2019-05-25 19:23:16 +0700

    detail page

--- pages/detail.js
+++ pages/detail.js
@@ -1,16 +1,46 @@
 import React, { Component } from 'react'
-import { Card, CardHeader, Container, Row, Col } from 'reactstrap'
+import { Card, CardHeader, Container, Row, Col, Table } from 'reactstrap'
 
 
 export default class detail extends Component {
   render() {
     return (
       <Container>
-        <Row  style={{ alignContent: 'center', backgroundColor:'pink' }}>
-          <div>
+        <Row>
+          <h1 style={{ alignSelf: 'center', alignSelf:'center'}}>
             ตารางการเผา
-          </div>
+          </h1>
         </Row>
+        <Table bordered>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Username</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td>Larry</td>
+            <td>the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </Table>
       </Container>
     )
   }
