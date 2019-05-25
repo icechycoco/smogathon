a1a40f6897071d23d7bceccf0d8bbd24160e5c58
icechycoco <icechycoco@gmail.com>
2019-05-25 19:17:02 +0700

    ..

---
+++ pages/detail.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import { Card, CardHeader, Container, Row, Col } from 'reactstrap'
+
+
+export default class detail extends Component {
+  render() {
+    return (
+      <Container>
+        <Row  style={{ alignContent: 'center', backgroundColor:'pink' }}>
+          <div>
+            ตารางการเผา
+          </div>
+        </Row>
+      </Container>
+    )
+  }
+}
